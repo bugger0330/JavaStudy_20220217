b040fd2c084298ae46464933e66a672d65dd4aea
qwer1qaaa <bugger0330@naver.com>
2022-02-21 11:17:08 +0900

    세번째 수업 연산자 내용

---
+++ JavaStudy_7290/src/a03_연산자/Operation01.java
@@ -0,0 +1,17 @@
+package a03_연산자;
+
+public class Operation01 {
+	public static void main(String[] args) {
+		int score = 10;
+//		score = score + 1;
+//		++score;//선 증가 
+//		score++;//후 증가
+		System.out.println(score++);
+		System.out.println(score);//그 다음번쨰에 증가 시켜라
+		
+		/**
+		 * System.out.println(score++); 는
+		 * System.out.println(++score); 라는 말이다
+		 */
+	}
+}
