f70906503bb8faf0152f6039505ba213fe14bb0e
qwer1qaaa <bugger0330@naver.com>
2022-02-21 11:14:56 +0900

    두번째 시간 변수에 대한 수업내용

---
+++ JavaStudy_7290/src/a02_변수/Cat.java
@@ -0,0 +1,24 @@
+package a02_변수;
+
+public class Cat {
+	public static void main(String[] args) {
+//		고양이 모양 출력
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+//		\    /\
+//		 )  ( ')
+//		(  /  )
+//		 \(__)|
+//		역슬러시 \는 \\ 두번 입력해야 인식된다
+		System.out.println();
+		System.out.println("|\\_/|");
+		System.out.println("|q p|    /}");
+		System.out.println("( 0 )\"\"\"\"\\");
+		System.out.println("|\"^\"\'    |");
+		System.out.println("||_/=\\\\__|");
+//		따옴표 출력 하려면 앞에 역슬러시 \ 를 붙이면 된다
+		
+	}
+}
---
+++ JavaStudy_7290/src/a02_변수/StudentInfo.java
@@ -0,0 +1,8 @@
+package a02_변수;
+
+public class StudentInfo {
+	public static void main(String[] args) {
+		long studentYear=333300000000l;
+		System.out.println(studentYear);
+	}
+}
